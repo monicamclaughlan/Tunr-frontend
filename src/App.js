9f066865d5502c54fc76ef23169b164c562cfc0e
Monica McLaughlan <monicamclaughlan@MacBook-Air.Home>
2021-05-20 16:09:09 -0400

    app.js fix

--- src/App.js
+++ src/App.js
@@ -4,26 +4,7 @@ import FavoriteSongs from "./FavoriteSongs";
 import AddSong from "./AddSong";
 import SongList from "./SongList";
 
-function App() {
-  const emptySong = {
-    title: "",
-    artist: "",
-    time: "",
-    favorite: false,
-  };
-  const [songs, setSongs] = useState([emptySong]);
-
-  return (
-    <div className="App">
-      <SongList songs={songs} />
-      <FavoriteSongs songs={songs} />
-      {/* <AddSong songs={songs} label="create" /> */}
-import './App.css';
-import React, {useState, useEffect} from "react"
-import FavoriteSongs from "./FavoriteSongs"
-import AddSong from './AddSong'
-import SongList from './SongList'
-
+   
 function App() {
 const url = "https://songs-329-tunr-backend.herokuapp.com"
 
